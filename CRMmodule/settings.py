f82608d6b1f3e4ee59ee7244262d94c29e6259b9
Silian1234 <isilinkina25565137@gmail.com>
2024-11-24 19:45:56 +0500

    Bearer Token Auth

--- CRMmodule/settings.py
+++ CRMmodule/settings.py
@@ -109,11 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'CRMmodule.authentication.BearerTokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
+
     ],
 }
 
